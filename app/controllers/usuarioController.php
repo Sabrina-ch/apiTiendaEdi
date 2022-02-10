aae201dca9bc5878acf51f327f7d6eb5d98abcbe
SabrinaChavez <chavez.sabrina@live.com>
2022-02-10 08:02:03 -0300

    login

--- app/controllers/usuarioController.php
+++ app/controllers/usuarioController.php
@@ -5,15 +5,15 @@
     public function retornarUsuario( $request,$response, $args){
        
 
-        $postdata = file_get_contents("php://input");
+       /* $postdata = file_get_contents("php://input");
         $request = json_decode($postdata);
         $nombre= $request->nombre;
         
         
         
-        $listaUsuario= Usuario::buscarUsuario();
+        $listaUsuario= Usuario::buscarUsuario();*/
        /* var_dump($listaUsuario);*/
-       $respuesta = [
+      /* $respuesta = [
         'success' => false,
         'message' => "Usuario o contraseña inválidos"
         ];
@@ -35,11 +35,38 @@
             
                    
           $response->getBody()->Write(json_encode($respuesta));
-          return $response;
+          return $response;*/
+
+          $parametros = $request->getParsedBody();
+        $arrayUsers = Usuario::buscarUsuario($parametros['nombre']);
+        
+        if(count($arrayUsers)==1){
+                $usersBD = new Usuario();
+                foreach($arrayUsers as $objUsuario){
+                    foreach ($objUsuario as $atr => $valueAtr) {
+                        $usersBD->{$atr} = $valueAtr;
+                    }
+                }
+                $passBD=$usersBD->pass;
+                if($usersBD->compararPass($listaDeParametros['clave'] , $passBD)){
+                    $response->getBody()->write("Acceso correcto");
+                }
+                else{
+                    $response->getBody()->write("Contraseña incorrecta");
+                }
         }
+        else{
+            $response->getBody()->write("Usuario incorrecto");
+        }
+  
+    return $response;
+
+    }
+
+        
     
 
-        public function registrarUsuario( $request,$response, $args){
+        /*public function registrarUsuario( $request,$response, $args){
        
              $postdata = file_get_contents("php://input");
                   $request = json_decode($postdata);
@@ -57,7 +84,7 @@
 
                     return $response;
      
-            }
+            }*/
 
         }
         
