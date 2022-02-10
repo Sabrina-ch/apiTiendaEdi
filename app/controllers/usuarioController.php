5e17d19ad5cead262e142456ba3bd725a4c2c7e0
SabrinaChavez <chavez.sabrina@live.com>
2022-02-10 08:37:30 -0300

    p

--- app/controllers/usuarioController.php
+++ app/controllers/usuarioController.php
@@ -2,19 +2,18 @@
 // el controlador va manejar cada una de las llamadas
  class UsuarioController{
 
-    public function retornarUsuario( $request,$response, $args){
+    public function retornarUsuario($request,$response,$args){
        
 
-        print("llegaste a retornar usuario");
-       /* $postdata = file_get_contents("php://input");
+        $postdata = file_get_contents("php://input");
         $request = json_decode($postdata);
         $nombre= $request->nombre;
         
         
         
-        $listaUsuario= Usuario::buscarUsuario();*/
+        $listaUsuario= Usuario::buscarUsuario();
        /* var_dump($listaUsuario);*/
-      /* $respuesta = [
+       $respuesta = [
         'success' => false,
         'message' => "Usuario o contraseña inválidos"
         ];
@@ -36,38 +35,16 @@
             
                    
           $response->getBody()->Write(json_encode($respuesta));
-          return $response;*/
+          return $response;
 
-         /* $parametros = $request->getParsedBody();
-        $arrayUsers = Usuario::buscarUsuario($parametros['nombre']);
-        
-        if(count($arrayUsers)==1){
-                $usersBD = new Usuario();
-                foreach($arrayUsers as $objUsuario){
-                    foreach ($objUsuario as $atr => $valueAtr) {
-                        $usersBD->{$atr} = $valueAtr;
-                    }
-                }
-                $passBD=$usersBD->pass;
-                if($usersBD->compararPass($listaDeParametros['clave'] , $passBD)){
-                    $response->getBody()->write("Acceso correcto");
-                }
-                else{
-                    $response->getBody()->write("Contraseña incorrecta");
-                }
-        }
-        else{
-            $response->getBody()->write("Usuario incorrecto");
-        }
-  
-    return $response;*/
+         
 
     }
 
         
     
 
-        /*public function registrarUsuario( $request,$response, $args){
+        public function registrarUsuario($request,$response,$args){
        
              $postdata = file_get_contents("php://input");
                   $request = json_decode($postdata);
@@ -85,7 +62,7 @@
 
                     return $response;
      
-            }*/
+            }
 
         }
         
